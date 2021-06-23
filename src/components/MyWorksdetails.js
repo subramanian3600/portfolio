08c959d32314978bea63c2f25a7596e615d306dd
SUBRAMANIAN S V <subramanian.s.v.2017.ece@ritchennai.edu.in>
2021-06-23 18:44:12 +0530

    added calc and foodie

--- src/components/MyWorksdetails.js
+++ src/components/MyWorksdetails.js
@@ -16,5 +16,24 @@ const Works = [
     link: "https://github.com/subramanian3600/TaskTodo",
     demo: "https://tasktodo-firebase.netlify.app",
   },
+  {
+    id: 3,
+    image: "https://www.linkpicture.com/q/calculator.png",
+    title: "Calculator",
+    category: "web_app",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link:"https://github.com/subramanian3600/Calculator",
+    demo: "https://calculate-math.vercel.app/",
+  },
+  {
+    id: 4,
+    image: "https://www.linkpicture.com/q/foodie.jpg",
+    title: "Foodie",
+    category: "web_app",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    link:"https://github.com/subramanian3600/Foodie",
+    demo: "https://foodie-search.vercel.app/",
+  },
+  
 ];
 export default Works;
